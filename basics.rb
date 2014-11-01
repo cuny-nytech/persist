75c19652cde07c8c2a4274265df91593467470e6
gato333 <gvoll333@gmail.com>
2014-11-01 19:17:17 -0400

    took out the need for the net/http gem, still havin problems with the db part, utilizing the datamembers and even knowing if im connectedd properly

--- basics.rb
+++ basics.rb
@@ -8,6 +8,20 @@ get '/' do
 	erb :index
 end
 
+post '/' do
+	conn= PG::Connection.new(host:'localhost',dbname:'singlepurp',port:'5432')
+	t=Time.now
+    t.getlocal("-04:00")
+    conn.exec_params("SELECT * FROM post ORDER BY posttime DESC LIMIT 1") do |result|
+        result.each do |row|
+            lastid=row.id
+        end
+    end
+    conn.exec_params("INSERT INTO post (id, postttime, username, avatar, userpost) VALUES ('#{lastid}', '#{t}', '#{params[:name]}', '#{params[:avatarimg]}', '#{params[:post]}')")
+
+	erb :index
+end
+
 get '/about' do 
 	erb :about
 end
