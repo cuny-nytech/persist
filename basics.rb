9dadeb40c1b24bd274c49e44dc6c199f3a6167e7
gato333 <gvoll333@gmail.com>
2014-11-03 00:22:57 -0500

    everything working and running, pagination, about, and contact page and links

--- basics.rb
+++ basics.rb
@@ -4,30 +4,41 @@ require 'sinatra'
 require 'pg'
 
 
-get '/' do 
-	erb :index
-end
-
-post '/' do
-	conn= PG::Connection.new(host:'localhost',dbname:'singlepurp',port:'5432')
-	t=Time.now
-    t.getlocal("-04:00")
-    conn.exec_params("SELECT * FROM post ORDER BY posttime DESC LIMIT 1") do |result|
-        result.each do |row|
-            lastid=row.id
-        end
-    end
-    conn.exec_params("INSERT INTO post (id, postttime, username, avatar, userpost) VALUES ('#{lastid}', '#{t}', '#{params[:name]}', '#{params[:avatarimg]}', '#{params[:post]}')")
-
-	erb :index
-end
-
 get '/about' do 
 	erb :about
 end
 
 get '/contact' do 
 	erb :contact
+end
+
+get '/' do 
+	erb :index
+end
+
+get '/:page' do
+	erb :index
+end
+
+post '/' do 
+	if(params[:name]!="" && params[:avatarimg] !="" && params[:post]!="" && params[:name]!="...person your impersonating here......" && params[:avatarimg] !="....(copy url address of image here....." )
+		conn= PG::Connection.new(host:'localhost',dbname:'singlepurp',port:'5432')
+		t=Time.now
+   		t.getlocal("-04:00")
+   		lastid=10
+   		conn.exec_params("SELECT * FROM post ORDER BY id DESC LIMIT 1") do |result|
+       		result.each do |row|
+          	  lastid=row['id'].to_i
+          	  lastid=lastid+1
+       		end
+    	end
+    	conn.exec_params("INSERT INTO post (id, posttime, username, avatar, userpost) VALUES ('#{lastid}', '#{t}', '#{params[:name]}', '#{params[:avatarimg]}', '#{params[:post]}')")
+    	params[:name]==""
+    	params[:avatarimg] =="" 
+    	params[:post]=="" 
+    end
+
+	erb :index
 end
 
 not_found do 
