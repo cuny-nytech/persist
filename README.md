bb1dea50d9cc96fcee2433210b95290845efebf8
gato333 <gvoll333@gmail.com>
2014-11-03 02:26:44 -0500

    coppied working persist into this repo

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-MY working HTML/CSS version of my website, for reference
+persist-ass
+===========
 
-i even pushed it on heroku : https://sleepy-atoll-6973.herokuapp.com/
+This repository creates a small web app that makes a small message board, like web app. 
+That will take in posts from anyone. 
+It is supposed to be a pretend watergate website for any famous person. 
+IE you can pretend to be Obama, Whitney Houston's Ghost, Eisnten, a Kardashien, or etc and say whatever you want. 
 
-since i cant get my ruby and the pg gem to work in the other one
-
+I have a sample of the website which works which is here (https://sleepy-atoll-6973.herokuapp.com/), but im having problems with pg and the ruby parts of the website, and getting it to run locally
--- basics.rb
+++ basics.rb
@@ -4,16 +4,42 @@ require 'sinatra'
 require 'pg'
 
 
-get '/' do 
-	erb :index
-end
-
 get '/about' do 
 	erb :about
 end
 
 get '/contact' do 
 	erb :contact
+end
+
+get '/' do 
+	erb :index
+end
+
+get '/:page' do
+	erb :index
+end
+
+post '/' do 
+	if(params[:name]!="" && params[:avatarimg] !="" && params[:post]!="" && params[:name]!="...person your impersonating here......" && params[:avatarimg] !="....(copy url address of image here....." )
+		conn= PG::Connection.new(host:'ec2-54-83-201-96.compute-1.amazonaws.com',user: 'tylozwhavlzite', password: 'hWIONURH3Ttr5dBfsSp1Urg_bc', dbname:'d7ceioa8vs7n4d',port:'5432')
+		t=Time.now
+   		t.getlocal("-04:00")
+   		lastid=0
+   		conn.exec_params("SELECT * FROM post ORDER BY id DESC LIMIT 1") do |result|
+       		result.each do |row|
+          	  lastid=row['id'].to_i
+          	  lastid=lastid+1
+       		end
+    	end
+    	conn.exec_params("INSERT INTO post (id, posttime, username, avatar, userpost) VALUES ('#{lastid}', '#{t}', '#{params[:name]}', '#{params[:avatarimg]}', '#{params[:post]}')")
+    	params[:name]==""
+    	params[:avatarimg] =="" 
+    	params[:post]=="" 
+    end
+    conn.close
+
+	erb :index
 end
 
 not_found do 
