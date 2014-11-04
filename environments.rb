a8bc381b2d58165eb0b1792879e90375bba37740
Bren Cavallo <bcavallo@gc.cuny.edu>
2014-11-03 23:06:38 -0500

    changed env

--- environments.rb
+++ environments.rb
@@ -4,12 +4,12 @@ configure :development do
 end
 
 configure :production do
-  db = URI.parse(ENV['DATABASE_URL'])
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
   ActiveRecord::Base.establish_connection(
     adapter: db.scheme == 'postgres' ? 'postgresql' : db.scheme,
     host: db.host,
     username: db.user,
-    passowrd: db.password,
+    password: db.password,
     database: db.path[1..-1],
     encoding: 'utf8'
   )
